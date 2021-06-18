3496ef641d843c328aea05b33b3a67e954c37afd
brunomartins32 <brunomartinns@hotmail.com>
2021-06-17 21:07:26 -0300

    Product-OrderItem one-to-many association

--- src/main/java/com/educandoweb/course/entities/pk/OrderItemPk.java
+++ src/main/java/com/educandoweb/course/entities/pk/OrderItemPk.java
@@ -12,10 +12,11 @@ import com.educandoweb.course.entities.Product;
 @Embeddable
 public class OrderItemPk implements Serializable {
 	private static final long serialVersionUID = 1L;
+	
 	@ManyToOne
 	@JoinColumn(name = "order_id")
-
 	private Order order;
+	
 	@ManyToOne
 	@JoinColumn(name = "product_id")
 	private Product product;
